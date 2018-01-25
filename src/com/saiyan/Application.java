250c0584d76bbaf6901a756ac92e380557c8def4
OlivierDoare <olivierdoare29@gmail.com>
2018-01-25 17:21:09 +0100

    Update Application.java

--- src/com/saiyan/Application.java
+++ src/com/saiyan/Application.java
@@ -3,7 +3,22 @@ package com.saiyan;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Creation of the body (add a session counter in the body)
+		Body olivier = new Body("Olivier", ...);
+		//setting the start parameters
+		olivier.Size.armSize = 23;
+		
+		
+		//Log a session
+		//This will probably go in an external class call "Session"
+		//Incrementation of the sessionNumber when you log a session
+		nb = olivier.sessionNumber;
+		olivier.sessionNumber += 1;
+		//Creation of a session Array or list...
+		Array session[] = new Array();
+		//enter 10 push-ups
+		olivier.pushUps(10)
+		session[nb].pushUps = numberOfPushUps
 
 	}
 
